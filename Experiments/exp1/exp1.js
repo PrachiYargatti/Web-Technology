2021c198c842b20a6a9dda80c6dad33319e36849
Prachi Yaragatti <yaragattiprachi@gmail.com>
2026-01-24 22:17:43 +0530

    experiment 1 updated

--- Experiments/exp1/exp1.js
+++ Experiments/exp1/exp1.js
@@ -1,97 +1,131 @@
-// const accountId = 101
-// accountId = 201
-// console.log(accountId) //TypeError : Assignment to constant variable
+console.log("=== Variable Declaration ===");
+document.writeln("=== Variable Declaration ===<br>");
 
-// let a = 10
-// var b = 20 
+const accountId = 1001;
+console.log("Account ID:", accountId);
+document.writeln("Account ID: " + accountId + "<br>");
 
-// {
-//     let a = 100
-//     var b = 200
-//     console.log(a, b) //100 200 
-// }
+// let & var
+let a = 10;
+var b = 20;
 
-// console.log(a, b)//10 200
+console.log("--- Block Scope Example ---");
+document.writeln("--- Block Scope Example ---<br>");
 
-// //	Standard numeric type.
-// let accNo = 5;
-// console.log(typeof(accNo));
+{
+    let a = 100;
+    let b = 200;
 
-// // sequence of characters.
-// let accName = "Prachi"
-// console.log(typeof(accName));
-
-// let isTrue = false
-// console.log(typeof(isTrue));
-
-// //Used for integers larger than \(2^{53}-1\).
-// let isBigInt = 100n
-// console.log(typeof(isBigInt));
-
-// let isNull = null 
-// console.log(typeof(isNull)); //object
-
-// // A variable that has been declared but not assigned a value.
-// let undefinedVar;
-// console.log(typeof(undefinedVar)); //undefined 
-
-// // Used to create unique, immutable identifiers.
-// let id = 45
-// let unique = Symbol(id)
-// console.log(typeof(unique)) //symbol
-
-// console.table([typeof(accNo), typeof(accName), typeof(isTrue), typeof(isBigInt), typeof(isNull), typeof(undefinedVar), typeof(unique)])
-// console.table([accNo, accName, isTrue, isBigInt, isNull, undefinedVar, unique])
-
-// //Activity 1)
-// //display students info with variables
-
-// let stu_name = "prachi"
-// let prn = '23UAM138'
-// let branch = 'AIML'
-// let year = 3
-// let age = 20
-// let email = 'prachi@gmail.com'
-// let add_city = 'ichalkaranji'
-
-// console.table({"name": stu_name, "prn": prn, "branch": branch, "year": year, "age": age, "email": email, "add-city": add_city})
-
-//activity 2)
-//odd even number check
-
-// Use prompt() instead of console.prompt()
-// Adding a '+' converts the string input into a number
-// const prompt = require('prompt-sync')();
-// let num = +prompt("Enter a number: ")
-
-// if (num % 2 == 0) {
-//     console.log(`${num} is even number`)
-// }       
-// else {
-//     console.log(`${num} is odd number`)
-// }
-
-// //Activity 3)
-// //student pass fail
-
-// const prompt = require('prompt-sync')();
-// marks = +prompt("Enter your marks: ")
-// if(marks >= 40){
-//     console.log("Student is pass")
-// }
-// else{
-//     console.log("Student is fail")
-// }
-
-// Activity 4)
-// display numbers using loop
-for(let i=1; i<=10; i++){
-    document.writeln(i+"<br>")
+    console.log("Inside Block:", a, b);
+    document.writeln("Inside Block: " + a + " " + b + "<br>");
 }
 
-// Activity 5)
-let x = 20
-let y = x 
-document.writeln(x, y) //20 20
-x = 200
-document.writeln(x, y) //200 20
+console.log("=== Data Types ===");
+document.writeln("=== Data Types ===<br>");
+
+let accId = 101;
+console.log("accId:", accId);
+document.writeln("accId: " + accId + "<br>");
+
+console.log("Type of accId:", typeof accId);
+document.writeln("Type of accId: " + typeof accId + "<br>");
+
+let accName = "Prachi";
+console.log("accName:", accName);
+document.writeln("accName: " + accName + "<br>");
+
+console.log("Type of accName:", typeof accName);
+document.writeln("Type of accName: " + typeof accName + "<br>");
+
+let isValid = true;
+console.log("isValid:", isValid);
+document.writeln("isValid: " + isValid + "<br>");
+
+console.log("Type of isValid:", typeof isValid);
+document.writeln("Type of isValid: " + typeof isValid + "<br>");
+
+let temp;
+console.log("temp:", temp);
+document.writeln("temp: " + temp + "<br>");
+
+console.log("Type of temp:", typeof temp);
+document.writeln("Type of temp: " + typeof temp + "<br>");
+
+let value = null;
+console.log("value:", value);
+document.writeln("value: " + value + "<br>");
+
+console.log("Type of value:", typeof value);
+document.writeln("Type of value: " + typeof value + "<br>");
+
+let sym1 = Symbol("id");
+console.log("Symbol:", sym1.toString());
+document.writeln("Symbol: " + sym1.toString() + "<br>");
+
+console.log("Type of sym1:", typeof sym1);
+document.writeln("Type of sym1: " + typeof sym1 + "<br>");
+
+let bigNumber = 1234567890123456789012345n;
+console.log("BigInt:", bigNumber);
+document.writeln("BigInt: " + bigNumber + "<br>");
+
+console.log("Type of bigNumber:", typeof bigNumber);
+document.writeln("Type of bigNumber: " + typeof bigNumber + "<br>");
+
+console.log("=== Activity 01 : Student Info ===");
+document.writeln("=== Activity 01 : Student Info ===<br>");
+
+let sname = "Prachi";
+let sroll = 101;
+let sage = 20;
+let isGirl = true;
+
+console.log("Name :", sname);
+document.writeln("Name : " + sname + "<br>");
+
+console.log("Roll Num :", sroll);
+document.writeln("Roll Num : " + sroll + "<br>");
+
+console.log("Age :", sage);
+document.writeln("Age : " + sage + "<br>");
+
+console.log("isGirl :", isGirl);
+document.writeln("isGirl : " + isGirl + "<br>");
+
+
+console.log("=== Activity 02 : Odd-Even ===");
+document.writeln("=== Activity 02 : Odd-Even ===<br>");
+
+let num1 = 10;
+
+if (num1 % 2 == 0) {
+    console.log(num1, "is Even Number");
+    document.writeln(num1 + " is Even Number<br>");
+} else {
+    console.log(num1, "is Odd Number");
+    document.writeln(num1 + " is Odd Number<br>");
+}
+
+console.log("=== Activity 03 : Pass-Fail ===");
+document.writeln("=== Activity 03 : Pass-Fail ===<br>");
+
+let marks = 50;
+
+if (marks > 40) {
+    console.log("Result: Pass");
+    document.writeln("Result: Pass<br>");
+} else {
+    console.log("Result: Fail");
+    document.writeln("Result: Fail<br>");
+}
+
+console.log("=== Activity 04 : Loop ===");
+document.writeln("=== Activity 04 : Loop ===<br>");
+
+console.log("First 10 Numbers:");
+document.writeln("First 10 Numbers:<br>");
+
+for (let i = 0; i < 10; i++) {
+    console.log("Number:", i);
+    document.writeln("Number: " + i + "<br>");
+}
